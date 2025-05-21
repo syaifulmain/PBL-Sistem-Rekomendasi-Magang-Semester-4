83ad2bc317d6f6668bed47668128b1db194b29cb
Muhamad Syaifullah <111264947+syaifulmain@users.noreply.github.com>
2025-05-21 07:07:23 +0700

    Update template.blade.php duplication csrf-token meta

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,6 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $page->title ?? $title ?? 'INI TITLE PAGE' }}</title>
